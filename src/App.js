e215fdf05fe058d52fef1f52b304c5428761b1b8
Kacper_Pietrusiak <kacperpietrusiak.biznes@gmail.com>
2023-03-17 20:46:23 +0100

    add tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Cell from "./components/Cell"
+import {useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [cells, setCells] = useState(["","","","","","","","",""])
+    const [go, setGo] = useState("circle");
+    const [winningMessage, setWinningMessage] = useState(null)
+
+    const message = "it is now " + go + " 's go";
+
+    const checkScore = () => {
+        const winningCombos = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ]
+
+        winningCombos.forEach(arr => {
+            let circleWin = arr.every(cell => cells[cell] === "circle")
+
+            if(circleWin){
+                setWinningMessage("Circle Wins")
+                return;
+            }
+        })
+
+        winningCombos.forEach(arr => {
+            let crossWin = arr.every(cell => cells[cell] === "cross")
+
+            if(crossWin){
+                setWinningMessage("Cross Wins")
+                return;
+            }
+        })
+    }
+
+    useEffect(() => {
+        checkScore()
+    },[cells])
+
+    return (
+        <div className="app">
+            <div className="gameboard">
+                {cells.map((cell, index) => (
+                    <Cell
+                        key={index}
+                        id={index}
+                        cell={cell}
+                        setCells={setCells}
+                        go={go}
+                        setGo={setGo}
+                        cells={cells}
+                        winningMessage={winningMessage}
+                    />
+                ))}
+            </div>
+            <p>{winningMessage || message}</p>
+        </div>
+    );
 }
 
 export default App;
